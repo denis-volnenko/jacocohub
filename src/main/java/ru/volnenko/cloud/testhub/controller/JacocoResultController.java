834045cd69a7197bd66f58dfccead7f8908477d8
Denis Volnenko <denis@volnenko.ru>
2025-10-14 22:09:15 +0400

    Refactoring jacoco controller.

--- src/main/java/ru/volnenko/cloud/testhub/controller/JacocoResultController.java
+++ src/main/java/ru/volnenko/cloud/testhub/controller/JacocoResultController.java
@@ -1,27 +1,17 @@
 package ru.volnenko.cloud.testhub.controller;
 
 import lombok.NonNull;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.volnenko.cloud.testhub.dto.JacocoResultDto;
 import ru.volnenko.cloud.testhub.dto.ResultDto;
-import ru.volnenko.cloud.testhub.service.JacocoResultService;
 
+@Controller
 @RequestMapping("/api/result/jacoco")
-public class JacocoResultController {
-
-    @Autowired
-    private JacocoResultService jacocoResultService;
+public interface JacocoResultController {
 
     @PostMapping(value = "/publish", produces = "application/json", consumes = "application/json")
-    public ResultDto publish(@NonNull final JacocoResultDto result) {
-        try {
-            jacocoResultService.publish(result);
-        } catch (@NonNull final Exception e) {
-            return new ResultDto(e);
-        }
-        return new ResultDto();
-    }
+    ResultDto publish(@NonNull JacocoResultDto result);
 
 }
---
+++ src/main/java/ru/volnenko/cloud/testhub/controller/JacocoResultControllerBean.java
@@ -0,0 +1,29 @@
+package ru.volnenko.cloud.testhub.controller;
+
+import lombok.NonNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.volnenko.cloud.testhub.dto.JacocoResultDto;
+import ru.volnenko.cloud.testhub.dto.ResultDto;
+import ru.volnenko.cloud.testhub.service.JacocoResultService;
+
+@Controller
+@RequestMapping("/api/result/jacoco")
+public class JacocoResultControllerBean implements JacocoResultController {
+
+    @Autowired
+    private JacocoResultService jacocoResultService;
+
+    @PostMapping(value = "/publish", produces = "application/json", consumes = "application/json")
+    public ResultDto publish(@NonNull final JacocoResultDto result) {
+        try {
+            jacocoResultService.publish(result);
+        } catch (@NonNull final Exception e) {
+            return new ResultDto(e);
+        }
+        return new ResultDto();
+    }
+
+}
