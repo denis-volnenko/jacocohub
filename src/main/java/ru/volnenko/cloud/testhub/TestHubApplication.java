8a52b098f016fdc647eb9a07fa09ade7a75084f1
Denis Volnenko <denis@volnenko.ru>
2025-10-14 21:14:39 +0400

    Added project.

---
+++ src/main/java/ru/volnenko/cloud/testhub/TestHubApplication.java
@@ -0,0 +1,13 @@
+package ru.volnenko.cloud.testhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/volnenko/cloud/testhub/TesthubApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.volnenko.cloud.testhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class TesthubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
